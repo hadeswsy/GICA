04cc70e5a01d6be8e24b316ba6114a9732dd06a9
colinphp <1165407848@qq.com>
2014-12-04 11:00:50 +0800

    1.3
    colin commit

---
+++ Application/Member/Controller/FriendsController.class.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Member\Controller;
+
+/**
+ * 文件控制器
+ * 主要用于下载模型的文件上传和下载
+ */
+
+class FriendsController extends MemberController {
+	
+	public function friends(){
+		$this->display();
+	}
+	
+}
